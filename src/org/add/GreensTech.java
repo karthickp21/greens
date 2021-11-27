d4819c8d9f07665a11ab7cbe8d710cd60e410be9
karthickp21 <karthick9325@gmail.com>
2021-11-27 15:55:07 +0530

    project structure

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,33 @@
+package org.add;
+
+public class GreensTech {
+	private void greensOmr() {
+		System.out.println("branch in omr");
+		}
+	private void greensAdayar() {
+		System.out.println("branch in adayar");
+		}
+	private void greensTambaram() {
+		System.out.println("branch in tambaram");
+}
+	private void greensVelachery() {
+	System.out.println("branch in velachery");
+
+}
+	private void annaNagar() {
+		System.out.println("branch in annaNagar");
+
+	}
+	public static void main(String[] args) {
+		GreensTech g=new GreensTech();
+		g.annaNagar();
+		g.greensAdayar();
+		g.greensOmr();
+		g.greensTambaram();
+		g.greensVelachery();
+		
+	}
+	
+    
+	
+}
